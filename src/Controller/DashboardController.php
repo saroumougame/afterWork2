4d6e2f3a79f7a44025bce0c9d6e0137bba6299b5
nicolas <n.tiesenhausen@gmail.com>
2018-06-26 18:52:54 +0200

    ajout du dashboard + debug de récuperation des informations

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sridar
+ * Date: 13/06/2018
+ * Time: 14:35
+ */
+
+namespace App\Controller;
+
+
+use App\Entity\UserGroupe;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Groupe;
+use App\Entity\User;
+use App\Entity\Event;
+use App\Entity\Entreprise;
+use App\Entity\Message;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\UserRepository;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+class DashboardController extends Controller
+{
+
+
+    public function showAction(Request $request)
+    {
+        
+        $User = $this->getUser();
+        $entityManager = $this->getDoctrine()->getManager();
+        $allusers = $entityManager->getRepository(User::class)->findAll();
+        $allGroupes = $entityManager->getRepository(Groupe::class)->findAll();
+        $allEvents = $entityManager->getRepository(Event::class)->findAll();
+        $allEntreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        $allMessages = $entityManager->getRepository(Message::class)->findAll();
+
+        $countUser = 0;
+        $tabUsers = [];
+         foreach ($allusers as $user) {
+            $countUser ++;
+            $tabUsers[$countUser]['username'] = $user->getUsername();
+            $tabUsers[$countUser]['email'] = $user->getEmail();
+            //$tabUsers[$countUser]['entreprise'] = $user->getEntreprise();
+            $tabUsers[$countUser]['entreprise'] = 'test';
+         }
+
+        $countGroupe = 0;
+        $tabGroupe = [];
+        foreach ($allGroupes as $groupe) {
+            $countGroupe ++;
+            $tabGroupe[$countGroupe]['nom'] = $groupe->getNom();            
+         } 
+
+        $countEvent = 0;
+        foreach ($allEvents as $event) {
+            $countEvent ++;
+         } 
+
+        $countEntreprises = 0;
+        foreach ($allEntreprises as $Entreprise) {
+            $countEntreprises ++;
+         } 
+
+        $countMessages = 0;
+        foreach ($allMessages as $Messages) {
+            $countMessages ++;
+         } 
+
+        return $this->render('Dashboard/index.html.twig', array(
+            'count' => $countUser,
+            'groupes' => $countGroupe,
+            'events' => $countEvent,
+            'Messages' => $countMessages,
+            'Entreprise' => $countEntreprises,
+            'tabUsers' => $tabUsers,
+            'tabGroupe' => $tabGroupe
+
+        ));
+
+    }
+
+/*
+// modification
+    public function accepterAction(UserGroupe $userGroupe){
+               $userGroupe->setStatue(1);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($userGroupe);
+                $entityManager->flush($userGroupe);
+
+        return $this->redirect($this->generateUrl('user_invite_notif'));
+
+}
+*/
+/*
+
+    public function refuserAction(UserGroupe $userGroupe){
+
+        // supprimer une entiter
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($userGroupe);
+        $entityManager->flush($userGroupe);
+        return $this->redirect($this->generateUrl('user_invite_notif'));
+
+    }
+    */
+}
