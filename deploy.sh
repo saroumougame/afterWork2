006f2822dd429aa7a7bf14af672d5b1e0416e3ee
Unknown <n.tiesenhausen@gmail.com>
2018-06-27 12:24:54 +0200

    add sshpass test 1.0

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # GNU bash, version 4.3.46
 
 echo "clef  ${key}";
-#sshpass -p 'password' ssh -o StrictHostKeyChecking=no root@vps554131.ovh.net;
+sshpass -p ${key} ssh -o StrictHostKeyChecking=no root@vps554131.ovh.net;
