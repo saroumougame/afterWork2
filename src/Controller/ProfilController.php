35285b68715623bf6bf72e4156db9fffcd561124
Unknown <n.tiesenhausen@gmail.com>
2018-06-30 16:27:20 +0200

    On affiche le premier event d'un utilisateur

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Groupe;
 use App\Entity\User;
+use App\Entity\Event;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\UserRepository;
 
@@ -24,10 +25,47 @@ class ProfilController extends Controller
     public function showAction()
     {
 
+    	$entityManager = $this->getDoctrine()->getManager();
+        $Groupes = $entityManager->getRepository(Groupe::class);
+        $Event = $entityManager->getRepository(Event::class);
+        $allUsersGroupes = $entityManager->getRepository(UserGroupe::class)->findAll();
+        $User = $this->getUser();
 
+
+        $param = array('user' => $this->getUser());
+        // j'ai tout les groupes d'un user
+        $groupes = $Groupes->getGroupeByUser($param);
+        // je prend le premier car je sais pas faire de jointure avec un "tableau de groupe"
+        $premierGroupe = $groupes[0]->getIdGroupe();
+
+        // je recupere tout ces events
+        $allEvents = $Event->getByGroupe($premierGroupe);
+        
+        // on récuperer un array avec la data de tout les events récupérés. 
+        $tabEvents = [];
+        $countEvents = 0;
+        foreach ($allEvents as $UnEvent) {
+            $tabEvents[$countEvents]['id'] = $UnEvent->getId();
+            $tabEvents[$countEvents]['titre'] = $UnEvent->getTitre();
+            $tabEvents[$countEvents]['message'] = $UnEvent->getMessage();
+            $tabEvents[$countEvents]['adress'] = $UnEvent->getAdress();
+            $tabEvents[$countEvents]['datedebut'] = $UnEvent->getDatedebut();
+            $tabEvents[$countEvents]['datefin'] = $UnEvent->getDatefin();
+            $countEvents++;
+        }
+
+
+            // il faut faire la jointure sur user et groupes
+        //var_dump($allUsersGroupes);
+
+    	$tabUser = [];
+    	$tabUser['username'] = $User->getUsername();
+    	$tabUser['email'] 	 = $User->getEmail();
+    	
 
         return $this->render('Profile/index.html.twig', array(
-            'toto' => 'lol',
+            'tabUser' => $tabUser,
+            'tabEvent' => $tabEvents
         ));
     }
 
--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -3,7 +3,7 @@
 
 namespace App\Repository;
 
-
+use App\Entity\Event;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
@@ -15,6 +15,21 @@ class EventRepository extends EntityRepository
     public function liste(){
 
 
+    }
+
+    public function getByGroupe($param){
+    	    
+    	    $qb = $this->getEntityManager()->createQueryBuilder()
+            ->select('e')
+           /* ->addSelect('d.id as idgroupe')
+            ->addSelect('d.nom AS nomGroupe')*/
+            ->from('App:Event', 'e')
+            ->join('e.groupe', 'g') // , col 60 near 'ug': Error: Class App\Entity\Groupe has no association named relationsUserGroupe
+            ->where('g.idGroupe = :groupe')
+        ->setParameter('groupe', $param);
+
+
+        return $qb->getQuery()->getResult();
     }
 
 }
